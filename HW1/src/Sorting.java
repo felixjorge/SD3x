61159e620700845009a838ee8bc7400081359cc4
luonglearnstocode <luong.nguyen@metropolia.fi>
2017-11-09 20:22:25 +0200

    hw1 part 1 Mergesort

--- HW1/src/Sorting.java
+++ HW1/src/Sorting.java
@@ -11,7 +11,43 @@ public class Sorting {
 	 */
 	public static void mergeSort(CompareInt[] arr) {
 		//TODO
+		CompareInt[] aux = new CompareInt[arr.length];
+		sort(arr, aux, 0, arr.length - 1);
 	}
+	
+	public static void sort(CompareInt[] arr, CompareInt[] aux, int low, int high) {
+		if (high <= low) return;
+		int mid = (high + low) / 2; // = low + (high - low) / 2
+		sort(arr, aux, low, mid);
+		sort(arr, aux, mid + 1, high);
+		merge(arr, aux, low, mid, high);
+	}
+	
+	public static void merge(CompareInt[] arr, CompareInt[] aux, int low, int mid, int high) {
+		for (int i = 0; i <= high; i++) {
+			aux[i] = arr[i];
+		}
+		
+		int i = 0; int j = mid + 1; int k = 0;
+		
+		while (i <= mid && j <= high) {
+			int comp = aux[i].compareTo(aux[j]);
+			if (comp < 0) {
+				arr[k++] = aux[i++];
+			} else {
+				arr[k++] = aux[j++];
+			}
+		}
+		
+		while (i <= mid) {
+			arr[k++] = aux[i++];
+		}
+		while (j <= high) {
+			arr[k++] = aux[j++];
+		}
+	}
+	
+	
 	
 	/**
 	 * Implement the quickSelect
